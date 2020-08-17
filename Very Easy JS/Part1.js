52589c8ef72ef175bfb8a15caebd8fbe7b611e98
Kyra Mellema <kemellema@gmail.com>
2020-08-17 08:07:09 -0400

    Added morning exercises from 8/17

--- Very Easy JS/Part1.js
+++ Very Easy JS/Part1.js
@@ -4,3 +4,60 @@
 function addition(a, b) {
 	return a + b;
 }
+
+// ----------
+// Convert Minutes into Seconds
+//Write a function that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+	return minutes * 60;
+}
+
+// ----------
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function addition(num) {
+	return num + 1;
+}
+
+// ----------
+// Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+	return (base * height) / 2;
+}
+
+// ----------
+// Return Something to Me!
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+function giveMeSomething(a) {
+	return "something" + " " + a;
+}
+
+// ----------
+// Convert Hours into Seconds
+// Write a function that converts hours into seconds.
+
+function howManySeconds(hours) {
+	return hours * 3600;
+}
+
+// ----------
+// Return the First Element in an Array
+// Create a function that takes an array and returns the first element.
+
+function getFirstValue(arr) {
+	return arr[0];
+}
+
+// Using the .shift() prototype works as well.
+
+function getFirstValue(arr) {
+	return arr.shift(0);
+}
+
+// ----------
+//
