f92b7f4cca4948590cabfaa38f69e17e906389a1
Kyra Mellema <kemellema@gmail.com>
2020-08-30 08:41:41 -0400

    Added morning exercises from 8/30

--- Very Easy JS/Part1.js
+++ Very Easy JS/Part1.js
@@ -244,3 +244,32 @@ function shouldServeDrinks(age, onBreak) {
 }
 
 // ---------
+// Buggy Code
+// The challenge is to try and fix this buggy code, given the inputs true and false.
+// BEFORE
+function has_bugs(buggy_code) {
+	if (buggyCode) {
+		return 'sad days'
+	} else if {
+		return 'it's a good day'
+	}
+}
+
+// AFTER
+function has_bugs(buggy_code) {
+	if (buggy_code) {
+		return 'sad days'
+	} else{
+		return 'it\'s a good day'
+	}
+}
+
+// ---------
+// Upvotes vs Downvotes
+// Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+function getVoteCount(votes) {
+	return votes.upvotes - votes.downvotes;
+}
+
+// ---------
